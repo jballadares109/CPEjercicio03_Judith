8ebd215d19d41bf698f0f0fe0abc229deb383178
Erwing Hernandez <birc89@hotmail.com>
2023-05-05 10:16:15 -0600

    Agregar archivos de proyecto.

---
+++ CPEjercicio01_Judith/Computadora.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+//3.Crea una clase Computadora con los siguientes atributos:
+//– Capacidad disco(GB)
+//-Espacio Disponible(GB)
+//– encendido: booleano
+//Los métodos a añadir son:
+//– aniadirDatos(int datos): añade información al disco duro, si supera el espacio disponible del disco, el tamaño del disco será el máximo posible. Solo si esta encendida la Computadora.
+//– eliminarDatos(int datos): elimina información al disco duro, si el tamaño del disco es menor que 0, el tamaño del disco se quedara a 0. Solo si esta encendida la Computadora.
+//– encender(): enciende la Computadora y muestra un mensaje en pantalla de Bienvenida.
+//– apagar(): apaga la Computadora y muestra un mensaje en pantalla de despedida
+//— Computadora : Constructor.
+namespace CPEjercicio01_Judith
+{
+    class Computadora
+    {
+        int capacidad;
+        int disponible;
+        bool estado;
+        public Computadora() { }
+
+        public Computadora(int capacidad, int disponible, bool estado)
+        {
+            this.capacidad = capacidad;
+            this.disponible = disponible;
+            this.estado = estado;
+        }
+
+        public int Capacidad { get => capacidad; set => capacidad = value; }
+        public int Disponible { get => disponible; set => disponible = value; }
+        public bool Estado { get => estado; set => estado = value; }
+
+        public void AddDato(int datos)
+        {
+            if (this.Capacidad != 0 && estado == true)
+            {
+
+                if (datos <= this.capacidad)
+                {
+                    Console.WriteLine("Dato agregado correctamente...");
+                    this.Disponible = this.Capacidad - datos;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: El dato ingresado excede la disponibilidad del disco duro...");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("No hay espacio disponible para agregar el dato...");
+            }
+        }
+        public void Eliminardato()
+        {
+            Console.WriteLine("¿Esta segur@ que desea eliminar un dato?: S/N");
+            string resp = Console.ReadLine();
+            if (resp == "S" || resp == "s")
+            {
+                this.disponible = this.capacidad;
+            }
+            else
+            {
+                Console.WriteLine("\t\t\tOperacion cancelada...");
+            }
+        }
+
+        public void Encendido()
+        {
+            this.estado = true;
+            Console.WriteLine("\tComputadora Encendida\n\t\tBienevenid@");
+        }
+        public void Apagado()
+        {
+            Console.WriteLine("¿Esta segur@ que desea apagar lacomputadora?: S/N");
+            string resp = Console.ReadLine();
+            if (resp == "S" || resp == "s")
+            {
+                this.estado = false;
+                Console.WriteLine("\tComputadora Apagada\n\t\tHasta luego");
+            }
+            else
+            {
+                Console.WriteLine("\t\t\tOperacion cancelada...");
+            }
+        }
+    }
+}
+
---
+++ CPEjercicio01_Judith/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CPEjercicio01_Judith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computadora com = new Computadora();
+            char resp;
+            com.Encendido();
+            Console.WriteLine("Ingrese la capacidad del disco duro: ");
+            com.Capacidad = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Desea agregar un dato: ");
+                resp = char.Parse(Console.ReadLine());
+                if (resp == 's' || resp == 'S')
+                {
+                    Console.WriteLine("Ingrese un dato: ");
+                    int datos = int.Parse(Console.ReadLine());
+                    com.AddDato(datos);
+                }
+                else
+                {
+                    Console.WriteLine("La opcion ingresada es invalida...");
+                }
+            } while (resp != 's' || resp != 's');
+
+            Console.WriteLine("Desea eliminar un dato: ");
+            resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+
+                com.Eliminardato();
+            }
+            else
+            {
+                Console.WriteLine("La opcion ingresada es invalida...");
+            }
+            com.Apagado();
+
+            Console.ReadKey();
+        }
+    }
+}
